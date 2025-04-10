6a61c0efcb21bec3d5da6e152cbe7fc1dbb60fec
Szymon Rogala <rogalaszymon78@gmail.com>
2025-04-10 17:04:24 +0200

    Create api request and recive data

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 const port = 3000
 const apiKey = 'CG-MPVtg1V1hNTiDnjKA3NzgXBG';
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.json());
@@ -16,9 +17,22 @@ app.get("/",(req,res)=>{
     res.render("index.ejs")
 })
 
-app.post("/crypto", (req,res) =>{
+app.post("/crypto", async (req,res) =>{
     let divId = (req.body.id).toLowerCase();
     console.log(divId);
+
+    const result = await axios.get(`https://api.coingecko.com/api/v3/coins/markets`,{
+        headers:{
+            'x-cg-demo-api-key': apiKey,
+        },
+        params:{
+            vs_currency: 'usd',
+            ids: divId
+        }
+    }
+    )
+    const data = result.data
+    console.log(data)
 })
 
 app.listen(port, ()=>{
